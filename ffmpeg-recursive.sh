b493fe604be0d1ae1bf5160608e482b1062402c0
Luis Gonzalez <hadesbox@gmail.com>
2017-07-26 16:47:12 +0200

    more changes

--- ffmpeg-recursive.sh
+++ ffmpeg-recursive.sh
@@ -1,6 +1,19 @@
+# http://oioiiooixiii.blogspot.com.es/2016/08/ffmpeg-recursive-effects-of-stacking.html
 # Twelve stacked 'tblend=all_mode=difference128' filters.
 # Deblocking 'spp' and 'average' filters used to mimimise strobing effects.
 # Due to latency issues, the result in ffplay will differ from a ffmpeg rendering.
+
+# - all_mode=difference128
+# - all_mode=difference
+# - all_mode=multiply128
+# - all_mode=multiply
+# - c0_mode=difference128
+# - unknown 'average_test.mkv'
+# - unknown 'all_average_test.mkv'
+# - unknown 'difference_test.mkv'
+# - unknown 'difference128_test.mkv'
+
+
 
 rm -rf "$1_out.mp4"
 
