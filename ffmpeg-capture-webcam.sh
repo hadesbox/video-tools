0e8953d36c9b724c216a80813e31fa342daed299
Luis Gonzalez <hadesbox@gmail.com>
2017-09-20 13:14:41 +0200

    Adding webcam capture script

---
+++ ffmpeg-capture-webcam.sh
@@ -0,0 +1,7 @@
+# To list devices 
+# v4l2-ctl --list-devices   OR
+# To list device capabilities
+# ffmpeg -f v4l2 -list_formats all -i /dev/video0
+
+# To capture/encode
+ffmpeg -f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 output.mkv
